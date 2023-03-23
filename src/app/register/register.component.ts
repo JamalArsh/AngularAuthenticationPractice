b9b62c7efdc7464e4d1d9fbb3326a6e096824b0a
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2023-03-23 12:06:09 +0530

    auth service function added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private builder: FormBuilder, private toastr: ToastrService) {}
 
+  registrationForm = this.builder.group({
+    id: this.builder.control(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(5)])
+    ),
+    name: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.required),
+    email: this.builder.control(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
+    gender: this.builder.control('male'),
+    role: this.builder.control(''),
+    isActive: this.builder.control(false),
+  });
+
+  proceedRegistration() {
+    if (this.registrationForm.valid) {
+    } else {
+      this.toastr.warning('Please enter valid data');
+    }
+  }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
+  apiurl = 'http://localhost:300/user';
 
-  constructor() { }
+  GetAll() {
+    return this.http.get(this.apiurl);
+  }
+  GetByCode(code: any) {
+    return this.http.get(this.apiurl + '/' + code);
+  }
+  proceedRegister(inputData: any) {
+    return this.http.post(this.apiurl, inputData);
+  }
+
+  updateRegister(code: any, inputData: any) {
+    return this.http.put(this.apiurl + '/' + code, inputData);
+  }
 }

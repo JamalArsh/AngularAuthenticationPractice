046c4083ea5d1c1ed1b4fd7c135802fe93112ed9
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2023-03-23 14:20:12 +0530

    login page created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  userData: any;
+  constructor(
+    private builder: FormBuilder,
+    private toastr: ToastrService,
+    private service: AuthService,
+    private router: Router
+  ) {}
 
+  loginForm = this.builder.group({
+    username: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.required),
+  });
+
+  proceedLogin() {
+    if (this.loginForm.valid) {
+      //   this.service.proceedRegister(this.loginForm.value).subscribe((res) => {
+      //     this.toastr.success(
+      //       'Please contact admin for enable access',
+      //       'Registered Successfully'
+      //     );
+      //     this.router.navigate(['login']);
+      //   });
+      // } else {
+      //   this.toastr.warning('Please enter valid data');
+      this.service.GetByCode(this.loginForm.value.username).subscribe((res) => {
+        this.userData = res;
+        if (this.userData.password === this.loginForm.value.password) {
+          if (this.userData.isActive) {
+            sessionStorage.setItem('username', this.userData.id);
+            sessionStorage.setItem('userrole', this.userData.role);
+            this.router.navigate(['']);
+          } else {
+            this.toastr.error('Please contact admin', 'In active user');
+          }
+        } else {
+          this.toastr.error('Invalid credentials');
+        }
+      });
+    }
+  }
 }

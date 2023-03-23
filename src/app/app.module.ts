7ae19c4fbf2b3ad4665f8942f512d957b5415ab5
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2023-03-23 11:45:56 +0530

    components created and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { MaterialModule } from 'src/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserlistingComponent } from './userlisting/userlisting.component';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent, LoginComponent, HomeComponent, UserlistingComponent, UpdatepopupComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
